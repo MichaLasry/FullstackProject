c385b07f433ed7c73a0967486ef208d3b0b065fb
osnatatra <osnatatra@gmail.com>
2021-07-08 20:25:32 +0300

    updateUser-deleteUser-getAllUsers-functions-server

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose')
 const User =require('../models/user')
+const Magazine =require('../models/magazine')
+
 
 const createUser = async (req, res) => {
 
@@ -15,8 +17,73 @@ const createUser = async (req, res) => {
     } 
     catch (error) {
         res.json({status: 400, message: error})
-        console.log('cannot saved user', error);
+        console.log('cannot saved user', error)
+
     }
 }
 
-module.exports={createUser}
+const deleteUser = async (req, res) => {
+    let userId = req.params.userId
+    await User.findByIdAndDelete(userId, (error, user) => {
+        if (error)
+            res.send(error)
+        else {
+            if (user) {
+                console.log('deleted user..');
+                console.log(user.magazines);
+                try { 
+                    user.magazines.map(async magazine => {
+                    console.log('magazine:', magazine._id);
+                    await Magazine.findByIdAndDelete(magazine._id)
+                    console.log('deleted magazine..', magazine._id);
+                })
+                res.json(user)
+            }
+            catch{
+                console.log('cannot find magazine..' ,error);
+            }
+            } 
+            else {
+                res.send('cannot find user: ',userId)
+            }
+        }
+    })
+}
+2
+const getAllUsers = async (req, res) => {
+    console.log("getAllUsers..")
+    try {
+        let users = await User.find()
+        res.json(users)
+    } 
+    catch (error) {
+        res.json(error)
+    }
+}
+
+const updateUser = async (req,res)=>{
+    try {
+        console.log("updateUser...");
+        let userId=req.params.userId
+        console.log("params...",userId);
+        console.log("body...",req.body);
+         await User.findByIdAndUpdate(userId,req.body)
+        
+        // await user.save()
+        console.log('user saved... ')
+     
+        res.status(200).json(user)
+    }
+     catch (error) {
+        
+        res.json({status: 400, message: error})
+        console.log('cannot saved user', error)
+    }
+}
+module.exports=
+    {
+        createUser,
+        deleteUser,
+        getAllUsers,
+        updateUser
+    }
--- router/router.js
+++ router/router.js
@@ -3,7 +3,13 @@ const postFunctions = require('../controllers/post')
 
 const User =require('../controllers/user')
 
+// user 
 router.post('/createUser',User.createUser)
+router.delete('/deleteUser/:userId',User.deleteUser) //not finished
+router.get('/getAllUsers',User.getAllUsers)
+router.patch('/updateUser/:userId',User.updateUser)
+
+// post
 router.post('/createPost', postFunctions.createPost)
 router.get('/getPost/:postId', postFunctions.getPost)
 router.get('/getPosts', postFunctions.getPosts)
